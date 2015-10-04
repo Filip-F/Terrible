1584d503cbf74841c4f3a4342d677f72b0335703
Filip <Filip@129.97.79.145>
2015-10-04 11:07:12 -0400

    Fixed

--- src/ShuffleFolder.java
+++ src/ShuffleFolder.java
@@ -3,8 +3,8 @@ import java.util.Set;
 public class ShuffleFolder {
 	public static void main (String []  args){
 		
-		Set<File> adsf =  FileNavigator.getFiles(new File ("C:\\Users\\Filip\\Desktop\\Fuck\\"), ".*");
+		//Set<File> adsf =  FileNavigator.getFiles(new File ("C:\\Users\\Filip\\Desktop\\Fuck\\"), ".*");
 		for (int i = 0; i < 20; i++)
-			new File("C:\\Users\\Filip\\Desktop\\Fuck\\" + String.valueOf(i)).mkdir();
+			new File(System.getProperty("user.home") + "\\Desktop\\Fuck\\" + String.valueOf(i)).mkdir();
 	}
 }
