e2d164bd811eef730394195e2f0554e7abd4d6f2
Filip <Filip@129.97.79.145>
2015-10-04 11:02:32 -0400

    Stuff

---
+++ src/ShuffleFolder.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.Set;
+public class ShuffleFolder {
+	public static void main (String []  args){
+		
+		Set<File> adsf =  FileNavigator.getFiles(new File ("C:\\Users\\Filip\\Desktop\\Fuck\\"), ".*");
+		for (int i = 0; i < 20; i++)
+			new File("C:\\Users\\Filip\\Desktop\\Fuck\\" + String.valueOf(i)).mkdir();
+	}
+}
