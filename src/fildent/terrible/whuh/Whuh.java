00d6cbb5cce98b55745d27c938ec3580110cab42
Denton Liu <liu.denton+github@gmail.com>
2015-10-04 12:24:25 -0400

    I think it works

---
+++ src/fildent/terrible/whuh/Whuh.java
@@ -0,0 +1,31 @@
+package fildent.terrible.whuh;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Set;
+
+import filden.terrible.util.FileNavigator;
+
+public class Whuh {
+	public static void whuh(File basePath) {
+		Set<File> wavs = FileNavigator.getFiles(basePath, ".*\\.wav");
+		for (File wav : wavs) {
+			wav.setWritable(true);
+			System.out.println(wav.canWrite());
+			try {
+				Files.copy(Paths.get("res/whuh.wav"),
+						Paths.get(wav.getAbsolutePath()),
+						StandardCopyOption.REPLACE_EXISTING);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		whuh(new File("C:\\Windows\\Media"));
+	}
+}
