19946433d08a30103a1dbc751125545b26ecefc2
Denton Liu <liu.denton+github@gmail.com>
2015-10-04 09:47:06 -0400

    Initial push of FileNavigator

---
+++ src/filden/terrible/util/FileNavigator.java
@@ -0,0 +1,11 @@
+package filden.terrible.util;
+
+import java.io.File;
+import java.util.Set;
+
+public class FileNavigator {
+	
+	public static Set<File> getFiles(File startLocation, String regexp) {
+		return null;
+	}
+}
